447fe7ea2ec52a285bd5654641c0872834bde81a
Jakub  Dobrogowski <dobrogowskijakub@gmail.com>
2018-06-28 01:37:15 +0200

    Initialization with Spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (category_Name) VALUES ('American');
+INSERT INTO category (category_Name) VALUES ('Italian');
+INSERT INTO category (category_Name) VALUES ('Mexican');
+INSERT INTO category (category_Name) VALUES ('Fast Food');
+INSERT INTO unite_of_mesure (uom) VALUES ('Teaspoon');
+INSERT INTO unite_of_mesure (uom) VALUES ('Tablespoon');
+INSERT INTO unite_of_mesure (uom) VALUES ('Cup');
+INSERT INTO unite_of_mesure (uom) VALUES ('Ounce');
